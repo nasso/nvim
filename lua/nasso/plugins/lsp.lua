9ce7037e369462934e510de5d4258d151498ada3
nasso <nassomails@gmail.com>
2025-03-08 22:47:26 +0100

    feat: `:Vs` = `:vs`

--- lua/nasso/plugins/lsp.lua
+++ lua/nasso/plugins/lsp.lua
@@ -177,7 +177,7 @@ return {
           end,
           svelte = function()
             require('lspconfig').svelte.setup {
-              on_attach = function(client, bufnr)
+              on_attach = function(client)
                 vim.api.nvim_create_autocmd("BufWritePost", {
                   group = vim.api.nvim_create_augroup("svelte_ondidchangetsorjsfile", { clear = true }),
                   pattern = { "*.js", "*.ts" },
