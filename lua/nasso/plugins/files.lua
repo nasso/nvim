77c6aaf03f6ca70fdc3ef9502bbbc0072fc646e3
nasso <git@nasso.dev>
2025-07-22 15:31:26 +0200

    chore(harpoon): replace `append` with `add`
    am i unironically going to use conventional commit messages here now?

--- lua/nasso/plugins/files.lua
+++ lua/nasso/plugins/files.lua
@@ -57,7 +57,7 @@ return {
       harpoon:setup()
 
       vim.keymap.set("n", "<leader><leader>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-      vim.keymap.set("n", "<leader>p", function() harpoon:list():append() end)
+      vim.keymap.set("n", "<leader>p", function() harpoon:list():add() end)
       vim.keymap.set("n", "<leader>h", function() harpoon:list():select(1) end)
       vim.keymap.set("n", "<leader>j", function() harpoon:list():select(2) end)
       vim.keymap.set("n", "<leader>k", function() harpoon:list():select(3) end)
