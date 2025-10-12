10499b301eb6e5b3051c4a48e3e911b9fc9c95e7
nasso <git@nasso.dev>
2025-10-12 02:04:51 +0200

    feat(rust): add environment variable to set the target

--- lsp/rust_analyzer.lua
+++ lsp/rust_analyzer.lua
@@ -7,14 +7,26 @@ elseif vim.env.LSP_RUST_FEATURES then
   features = vim.split(vim.env.LSP_RUST_FEATURES, ",")
 end
 
+--- @type string | nil
+local target = nil
+
+if vim.env.LSP_RUST_TARGET then
+  target = vim.env.LSP_RUST_TARGET
+end
+
+
 return {
   settings = {
     ["rust-analyzer"] = {
       checkOnSave = true,
-      cargo = { features = features },
+      cargo = {
+        features = features,
+        target = target,
+      },
       check = {
         enable = true,
         command = "clippy",
+        features = features,
       },
     },
   },
