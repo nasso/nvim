4de9896d0547e833c7e827a87487b08b4d9e6680
nasso <git@nasso.dev>
2025-10-12 02:04:51 +0200

    Allow using ^K for digraphs in insert mode

--- lua/nasso/remap.lua
+++ lua/nasso/remap.lua
@@ -24,10 +24,10 @@ vim.keymap.set("i", "kJ", "<Esc>", { silent = true })
 vim.keymap.set("i", "KJ", "<Esc>", { silent = true })
 
 -- <C-[hjkl]> to move around windows
-vim.keymap.set({ "n", "i", "v" }, "<C-h>", "<C-w><C-h>", { silent = true })
-vim.keymap.set({ "n", "i", "v" }, "<C-j>", "<C-w><C-j>", { silent = true })
-vim.keymap.set({ "n", "i", "v" }, "<C-k>", "<C-w><C-k>", { silent = true })
-vim.keymap.set({ "n", "i", "v" }, "<C-l>", "<C-w><C-l>", { silent = true })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { silent = true })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { silent = true })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { silent = true })
 
 -- make some commands case insensitive because omfg!!!
 vim.cmd "cnoreabbrev W w"
