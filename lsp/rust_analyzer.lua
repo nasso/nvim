132d974c9f48231e3e7148a9bd6d7c1ec866ab53
nasso <git@nasso.dev>
2025-10-12 02:04:51 +0200

    feat(rust): remove `LSP_RUST_ANALYZER_FEATURES` variable
    I always use the shorter `LSP_RUST_FEATURES` anyway.

--- lsp/rust_analyzer.lua
+++ lsp/rust_analyzer.lua
@@ -1,9 +1,7 @@
 --- @type string | string[]
 local features = "all"
 
-if vim.env.LSP_RUST_ANALYZER_FEATURES then
-  features = vim.split(vim.env.LSP_RUST_ANALYZER_FEATURES, ",")
-elseif vim.env.LSP_RUST_FEATURES then
+if vim.env.LSP_RUST_FEATURES then
   features = vim.split(vim.env.LSP_RUST_FEATURES, ",")
 end
 
