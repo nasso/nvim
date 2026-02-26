52989464e54c720560a0a0df9adb7b50aef2f352
nasso <git@nasso.dev>
2026-02-26 14:29:52 +0100

    remove markdown preview plugin

--- lua/nasso/plugins/utils.lua
+++ lua/nasso/plugins/utils.lua
@@ -63,15 +63,4 @@ return {
       vim.keymap.set("n", "<leader>v", toggle, { noremap = true })
     end,
   },
-
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install && git restore .",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-      vim.g.mkdp_auto_close = 0
-    end,
-    ft = { "markdown" },
-  },
 }
